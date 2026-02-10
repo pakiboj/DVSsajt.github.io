911d202bddbb7f11f211a09b11a07a95696e308d
pakiboj <mihajlop2101@gmail.com>
2026-02-10 22:54:01 +0100

    dodatne izmene na sajt, dodao sam muzicku kutiju i mailbox

---
+++ scripts/player.js
@@ -0,0 +1,44 @@
+let i = 0;
+const player = document.getElementById("player");
+const files = [
+  "audio/Spiritual_Healing.mp3",
+  "audio/The_Philosopher.mp3",
+  "audio/u1,000_Eyes",
+];
+const riffs = [210, 76, 92];
+
+player.src = files[i];
+
+
+function Riff() {
+  Jump(riffs[i]);
+}
+
+function Jump(k){
+    if (player.readyState >= 1) {
+    player.currentTime = k;
+    } else {
+        player.addEventListener("loadedmetadata", () => {
+        player.currentTime = k;
+        }, { once: true });
+    }
+}
+
+function Next(){
+    i++;
+    if (i >= files.length) {
+        i = 0; // loop back to first song
+    }
+
+    player.src = files[i];
+    player.play();
+}
+
+function playPauseAudio() {
+    if (player.paused) {
+    player.play();
+    } else {
+    player.pause();
+    }
+}
+player.addEventListener("ended", Next);
